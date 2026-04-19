95c09fe2dd545e511e1229aecc8b384614c02df3
Ryle-Peter-May <230333907@mycput.ac.za>
2026-04-19 15:17:16 +0200

    Update database.php

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$dbname = "cput_stays";
-$username = "root";
-$password = "A1z#334me";
+$host = "sql213.infinityfree.com";
+$dbname = "if0_41700525_cput_stays";
+$username = "if0_41700525";
+$password = "14kszhIVx0WH";
 
 $mysqli = new mysqli(hostname: $host,
                      username: $username,
